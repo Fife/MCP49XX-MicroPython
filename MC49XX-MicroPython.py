4149e532c6b4b3f10c159e4e25079efdb8f69248
Jacob Fifield <43118015+GermanWaffles@users.noreply.github.com>
2021-05-02 11:24:47 -0600

    Update MC49XX-MicroPython.py
    removed ubitstring dependency.
    Fixed frames not being generated correctly.
    Fixed chip not responding.
    Implemented all control methods

--- MC49XX-MicroPython.py
+++ MC49XX-MicroPython.py
@@ -1,9 +1,3 @@
-#Dependacies: 
-    #machine
-    #ubitString
-    #micropython-types
-    #micropython-copy
-    
 # A Class that is a "digital instance" of the MCP49XX DAC. This is essentially 
 # a wrapper class that abstracts some of the interfacing away from the user so that
 # people new to microcontrollers can get an easily available and well-documented DAC
@@ -19,54 +13,36 @@
         #LDAC Latch DAC Input - Default Pin: N/A(Must be defined) 
     #VRef - 3.3V-5V (3.3V Default)
 
-#Controls/Methods: 
-    #Write to the DAC 
-    #Buffered/Unbuffered Mode
-    #Output Gain Selecetion (x1, x2)
-    #Shutdown Control
+#Implementation TODOS: 
+    #VRef Gaurds so users can't change vRef beyond hardware specs 
+    #Write value method so user can write direct interger values
 
 #Desired interface example:
 
-#myObg = MCP() - Minimal Instantiation with all defaults
-#myObj = MCP(BITS, CS_PIN, SDI_PIN, LDAC, VREF, GAIN, BUFFER_MODE) - Instantiates instance of chip with all params in constructor
+#myObj = MCP("MCP4901", 9)
 #myObj.write(1.26) - Wrting values
 #myObj.setGainFlag(True) - Setting Gain
 #myObj.setBufferFlag(True)- Setting Buffer
-#myObj.shutdown(True)- Shutdown Control
+#myObj.shutdown()- Shutdown Control
+#myObj.shutdown()- Shutdown Control
 #myObj.setLDAC(PIN)- Set Latch DAC/Any other interface params
 
-#Implementation TODOS: 
-    #Setup Methods
-        #SPI superconstructor 
-        #Interface pin instantiation
-        #Vref 
-    #Control Methods 
-        #Write to the DAC 
-        #Buffered/Unbuffered Mode
-        #Output Gain Selecetion (x1, x2)
-        #Shutdown Control
 
-
-from machine import SPI
-from machine import SoftSPI
-from machine import ADC
-from machine import Pin
-#from ubitstring import *
+from machine import SoftSPI, ADC, Pin
 
 class MCP():
     def __init__(self, name, csPin):
-        if (name == "MCP4901"):
-            
-            self.spiInstance = SoftSPI(sck=Pin(6), mosi=Pin(7), miso=Pin(4))
+        if (name == "MCP4901"):       
+            self.spiInstance = SoftSPI(baudrate=1000000, sck=Pin(6), mosi=Pin(7), miso=Pin(4))
             self.dataBitSize = 8
             self.gain = True
             self.vRef = 3.3
-            self.bufferMode = False
+            self.bufferMode = True
             self.isOn = True
             self.chipSelectPin = Pin(csPin, Pin.OUT, Pin.PULL_UP)
             
         elif (name == "MCP4911"):
-            self.spiInstance = SoftSPI(sck=Pin(6), mosi=Pin(7), miso=Pin(4))
+            self.spiInstance = SoftSPI(baudrate=1000000, sck=Pin(6), mosi=Pin(7), miso=Pin(4))
             self.dataBitSize = 10
             self.gain = True
             self.vRef = 3.33
@@ -75,7 +51,7 @@ class MCP():
             self.chipSelectPin = Pin(csPin, Pin.OUT, Pin.PULL_UP)
             
         elif (name == "MCP4921"):
-            self.spiInstance = SoftSPI(sck=Pin(6), mosi=Pin(7), miso=Pin(4))
+            self.spiInstance = SoftSPI(baudrate=1000000, sck=Pin(6), mosi=Pin(7), miso=Pin(4))
             self.dataBitSize = 12
             self.gain = True
             self.vRef = 3.33
@@ -112,14 +88,3 @@ class MCP():
     
     def setBufferMode(self, flag):
         self.bufferMode = flag
-
-#Test code 
-C_SELECT = 9
-myMCP = MCP("MCP4901", C_SELECT)
-myMCP.writeVolt(2.9)
-
-value = machine.ADC(26)
-reading = value.read_u16()
-
-print(reading)
-
