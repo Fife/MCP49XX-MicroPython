d5d73982744829393df29bb4810d231eef925e89
Jacob Fifield <43118015+GermanWaffles@users.noreply.github.com>
2021-05-03 07:20:32 -0600

    Update example.py
    fixed camelCase variable

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ from MCP49XX import MCP
 
 C_SELECT = 9 #Chip Select Pin. This goes low when we write data to the MCP49XX Bus
 
-myMCP = MCP("MCP4901", C_SELECT) #Creation of an MCP object instance MCP(ChipName, ChipSelectPin)
+myMCP = MCP("MCP4901", C_SELECT) #Creation of an MCP object instance MCP(chipName, chipSelectPin)
 
 myMCP.writeVolt(1.65) #Writing a voltage value to the MCP49XX
 
